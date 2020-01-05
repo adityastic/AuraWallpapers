bc82824ec9284fdf5c20f00ca98b2543c36db731
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-01-05 19:36:14 +0000

    chore: fix build

--- app/src/main/java/wallpapers/aura/Views/SquareImageView.java
+++ app/src/main/java/wallpapers/aura/Views/SquareImageView.java
@@ -4,12 +4,13 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+
+import androidx.appcompat.widget.AppCompatImageView;
 
 /**
  * Created by ${Aditya} on 7/27/2016.
  */
-public class SquareImageView extends ImageView{
+public class SquareImageView extends AppCompatImageView {
 
     public SquareImageView(Context context) {
         super(context);
@@ -23,10 +24,6 @@ public class SquareImageView extends ImageView{
         super(context, attrs, defStyleAttr);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
